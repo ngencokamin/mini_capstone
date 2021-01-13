41f142ed4e746de1acf7cda795cb93b51039ae53
nix <ngencokamin@gmail.com>
2021-01-13 12:34:06 -0800

    Adds route and method for showing single product, almost displays single item (error needs fixing before next commit)

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -3,4 +3,9 @@ class Api::ProductsController < ApplicationController
     @products = Product.all
     render "show_all_products.json.jb"
   end
+
+  def show_single
+    @products = Product.first
+    render "show_single_product.json.jb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "/all_products" => "products#show_all"
+    get "/single_product" => "products#show_single"
   end
 end
