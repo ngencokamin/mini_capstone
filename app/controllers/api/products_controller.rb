7b2ab96efc15edbd5b30bcf693d7007e622737b6
nix <ngencokamin@gmail.com>
2021-01-13 12:30:40 -0800

    Adds view, ability to display all products

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,6 @@
 class Api::ProductsController < ApplicationController
+  def show_all
+    @products = Product.all
+    render "show_all_products.json.jb"
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+new_product = Product.create(name: "Lightsaber", price: 500000, image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5OU572Sjy-_mgWWNnuRKn0BXeUhNiMzau3Q&usqp=CAU", description: "The tool of force wielders, this product can cut through almost anything. DISCLAIMER: blades are weightless with heavy handles and high momentum. We are not liable for any non-force user dismemberment.")
+new_product = Product.create(name: "Stick", price: 1, image_url: "https://www.toyhalloffame.org/sites/www.toyhalloffame.org/files/toys/square/stick_0.png", description: "What's brown and sticky?")
+new_product = Product.create(name: "Horn of Gondor", price: 350, image_url: "https://static.wikia.nocookie.net/lotr/images/4/4f/BoromirHorn.jpg/revision/latest/scale-to-width-down/249?cb=20070419194711", description: "A sound of hope in the warring Middle Earth. Pretty much just annoying here, though.")
+new_product = Product.create(name: "Master Ball", price: 150, image_url: "https://i.ebayimg.com/images/g/wFUAAOSw64ZeFTJ7/s-l400.jpg", description: "Can catch any pokemon without fail. We assume it works on animals too. Probably.")
