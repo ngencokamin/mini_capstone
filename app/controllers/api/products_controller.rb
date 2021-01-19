4401e4a0d4f5e29bdd047241f0fa5c8ddc1896bd
nix <ngencokamin@gmail.com>
2021-01-19 12:05:29 -0800

    Adds formatting and partial for returning data

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -21,6 +21,23 @@ class Api::ProductsController < ApplicationController
     render "show.json.jb"
   end
 
+  def update
+    param_id = params[:id]
+    @product = Product.find_by(id: param_id)
+    @product.name = params[:name] || @product.name
+    @product.price = params[:price] || @product.price
+    @product.image_url = params[:image_url] || @product.image_url
+    @product.description = params[:description] || @product.description
+    @product.save
+    render "show.json.jb"
+  end
+
+  def destroy
+    product = Product.find_by(id: params[:id])
+    product.destroy
+    render json: {message: "Your product has now been obliterated."}
+  end
+
   # def any_product_action
   #   which = params[:item].to_i
   #   @product = Product.find_by(id: which)
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
     get "/products" => "products#index"
     get "/products/:id" => "products#show"
     post "/products" => "products#create"
+    patch "/products/:id" => "products#update"
+    delete "/products/:id" => "products#destroy"
     # get "/single_product" => "products#show_single"
     # get "/single_product_query" => "products#any_product_action"
     # get "/single_product_segment/:item" => "products#any_product_action"
